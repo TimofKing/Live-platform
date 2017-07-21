dd336da8657a01ea833c58fecfff009dfc2510ad
TimofKing <bkchen0800@gmail.com>
2017-07-21 23:12:03 +0800

    Twitch

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ watch = require('gulp-watch')     //引入watch套件
 
 gulp.task('serverGO', function () {
     connect.server({
-        root: 'build', //設定開啟位置
+        root: 'build/LOL_Live.html', //設定開啟位置
         livereload: true, //開啟livereload
         port: 8787 //設定預設port為8787
     });
